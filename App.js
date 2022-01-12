552652e8390ac0e33f37d18f365f21c60ad83c88
kleitong1 <44074264+kleitong1@users.noreply.github.com>
2022-01-12 03:24:35 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+
+import { StatusBar, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
+
+export default function App() {
+
+  return (
+    <SafeAreaView>
+
+
+      <StatusBar />
+      <Cesta />
+
+    </SafeAreaView>
+  );
+}
+
+
---
+++ src/telas/Cesta.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// Pegando componente que foi exportado do react native
+import { Image, StyleSheet, Text, Dimensions } from 'react-native'
+
+import topo from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width;
+
+export default function Cesta(){
+    return <>
+    <Image source= {topo} style={estilos.topo} />
+    <Text style={estilos.titulo}>Detalhes da Cesta </Text>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%" ,
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 32,
+        color: "white",
+        fontWeight: "bold",
+        padding: 32
+    }
+})
