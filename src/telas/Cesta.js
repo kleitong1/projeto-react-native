b0ab3bd1d8f1feb05dfb08508f2955e51f53ca78
kleitong1 <44074264+kleitong1@users.noreply.github.com>
2022-01-12 22:58:43 -0300

    alteracao de propriedades e cores

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,24 +1,44 @@
 import React from 'react';
 
 // Pegando componente que foi exportado do react native
-import { Image, StyleSheet, Text, Dimensions } from 'react-native'
+import { View, Image, StyleSheet, Text, Dimensions } from 'react-native'
+
 
 import topo from '../../assets/topo.png'
+import logo from '../../assets/logo.png'
 
 const width = Dimensions.get('screen').width;
 
-export default function Cesta(){
+export default function Cesta() {
+
+
     return <>
-    <Image source= {topo} style={estilos.topo} />
-    <Text style={estilos.titulo}>Detalhes da Cesta </Text>
+        <Image source={topo} style={estilos.topo} />
+        <Text style={estilos.titulo}>Detalhes da Cesta </Text>
+
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image source={logo} style={estilos.imagemFazenda} />
+                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}> Uma cesta com produtos selecionados
+                cuidadosamente da fazenda
+                direto para a cozinha
+            </Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
+
     </>
 }
 
 const estilos = StyleSheet.create({
     topo: {
-        width: "100%" ,
+        width: "100%",
         height: 578 / 768 * width,
+        fontFamily: "Montserrat"
     },
+
     titulo: {
         width: "100%",
         position: "absolute",
@@ -27,5 +47,52 @@ const estilos = StyleSheet.create({
         color: "white",
         fontWeight: "bold",
         padding: 32
+    },
+
+    cesta: {
+        paddingVertical: 16,
+        paddingHorizontal: 32,
+
+    },
+
+    nome: {
+        color: "#464646",
+        padding: 6,
+        fontSize: 50,
+        lineHeight: 42,
+        fontWeight: "bold",
+
+    },
+
+    fazenda: {
+        flexDirection: "row",
+        paddingVertical: 12,
+    },
+
+
+    imagemFazenda: {
+        width: 64,
+        height: 62,
+    },
+
+    nomeFazenda: {
+        fontSize: 32,
+        lineHeight: 52,
+        marginLeft: 24,
+    },
+
+    descricao: {
+        color: "#A3A3A3",
+        fontSize: 32,
+        lineHeight: 52
+    },
+
+    preco: {
+        padding: 32,
+        color: "#2a9b32",
+        fontWeight: "bold",
+        fontSize: 52,
+        lineHeight: 42,
+        marginTop: 16
     }
 })
